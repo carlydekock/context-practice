2704cc585be4bd5d717fa1cfb93e7af83947ba74
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-08-17 16:16:35 -0700

    created context and provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import NavBar from "./components/NavBar";
 import Form from "./components/Form";
+import PageContent from "./components/PageContent";
+import { ThemeProvider } from './context/ThemeContext';
 
 class App extends Component {
   render() {
     return (
-      <>
-        <NavBar/>
-        <Form />
-      </>
-    )
+      <ThemeProvider>
+        <PageContent>
+          <NavBar />
+          <Form />
+        </PageContent>
+      </ThemeProvider>
+    );
   }
 }
 
