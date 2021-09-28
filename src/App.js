c66b1ab5c997362176e82a7fa740fa5f3be692f2
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-09-28 16:30:10 -0700

    implemented functional components and hooks in rest of code, eliminated class based components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import NavBar from "./components/NavBar";
 import Form from "./components/Form";
 import PageContent from "./components/PageContent";
 import { ThemeProvider } from './context/ThemeContext';
 import { LanguageProvider } from './context/LanguageContext';
 
-class App extends Component {
-  render() {
-    return (
-      <ThemeProvider>
-        <LanguageProvider>
-          <PageContent>
-            <NavBar />
-            <Form />
-          </PageContent>
-        </LanguageProvider>
-      </ThemeProvider>
-    );
-  }
+function App() {
+  return (
+    <ThemeProvider>
+      <LanguageProvider>
+        <PageContent>
+          <NavBar />
+          <Form />
+        </PageContent>
+      </LanguageProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
